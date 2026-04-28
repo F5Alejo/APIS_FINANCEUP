8fd44d359d517e3f0eb14cde89356575f0499cb0
F3SHARITH <dahiana.bermudez2007@gmail.com>
2026-04-28 15:35:35 -0500

    se crean los archivos go.mod, go.sum y la tabla asesor_bancario

---
+++ NEGOCIO/controllers/helpers.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// respondJSON es el helper global para respuestas JSON
+func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(payload)
+}
