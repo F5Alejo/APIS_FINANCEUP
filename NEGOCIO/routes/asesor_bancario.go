a91906d829f6f7d8b56b46e3e5639b5966f617e3
F3SHARITH <dahiana.bermudez2007@gmail.com>
2026-04-28 15:52:44 -0500

    se crean las rutas de la tabla asesor_bancario

---
+++ NEGOCIO/routes/asesor_bancario.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"API_GO_CRUD/controllers"
+	"github.com/gorilla/mux"
+)
+
+func RegisterAsesorRoutes(r *mux.Router) {
+	r.HandleFunc("/negocio/asesores", controllers.GetAllAsesores).Methods("GET")
+	r.HandleFunc("/negocio/asesores/{id}", controllers.GetAsesorByID).Methods("GET")
+	r.HandleFunc("/negocio/asesores", controllers.CreateAsesor).Methods("POST")
+	r.HandleFunc("/negocio/asesores/{id}", controllers.UpdateAsesor).Methods("PUT")
+	r.HandleFunc("/negocio/asesores/{id}", controllers.DeleteAsesor).Methods("DELETE")
+}
